0385e478add5d926b6cd753ecca93120086c0ff9
Robson <robsoncezariosilva@gmail.com>
2020-10-28 00:11:23 -0300

    Commit inicial

---
+++ app/build/precache-manifest.85c0e55492970b50d839ad0d2537641f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8e064b90b11f24d6a0bdbb94eb14e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e581a71d31388f5326bd",
+    "url": "./static/css/main.aadfaa00.chunk.css"
+  },
+  {
+    "revision": "583f77c379d275c76868",
+    "url": "./static/js/2.93e89bb4.chunk.js"
+  },
+  {
+    "revision": "ebef0e3b273ae17524922cb1eb15b90a",
+    "url": "./static/js/2.93e89bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e581a71d31388f5326bd",
+    "url": "./static/js/main.3ceb9124.chunk.js"
+  },
+  {
+    "revision": "bf8730eeac732d47c378",
+    "url": "./static/js/runtime-main.dfbba2b2.js"
+  }
+]);
